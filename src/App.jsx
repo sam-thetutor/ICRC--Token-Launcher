4e10eec3fe417638b723acf0e2ba2731e20aff9c
sam-thetutor <atwebembeiresamuel2020@gmail.com>
2024-05-09 15:44:01 +0300

    started template fot the launcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import Dashboard from './components/Dashboard';
-import Footer from './components/Footer';
 function App() {
 
   return (
     <>
-    <Dashboard/>
-    </>
+Home    </>
   
   );
 }
